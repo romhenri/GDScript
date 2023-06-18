978c2d257ca4e43c58805de7217f346049b14494
Rômulo Henri <123867521+romhenri@users.noreply.github.com>
2023-06-17 23:19:02 -0300

    study: day-01

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,8 +1,16 @@
 extends Node2D
 
+# Variable
+var engine = "Godot" #String
+var msg01: String = "Olá, viajante, em que posso lhe ajudar?" #String
+
 # The function "_ready():" is executed whe the object is ready 
 func _ready():
 	print("Hello, world!")
-	print("Hello, world!")
-	
+	# Same mode of JavaScript to concatenate
+	print("I'm studying " + engine) 
+	print(engine.is_empty())
+	print(engine.length())
+	print(engine.to_upper()) 
 # This is a comment
+
