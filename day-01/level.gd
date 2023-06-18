9225288419256fd2d55a7949d7b2ef8c4bb31b57
Rômulo Henri <123867521+romhenri@users.noreply.github.com>
2023-06-18 08:53:25 -0300

    study: day-02

--- day-01/level.gd
+++ day-01/level.gd
@@ -3,14 +3,18 @@ extends Node2D
 # Variable
 var engine = "Godot" #String
 var msg01: String = "Olá, viajante, em que posso lhe ajudar?" #String
+var msg02: String = "Sua pontuação é de " #String
+var msg02_1: String = "pontos." #String
+var score: int = 22 #Number: Int and Float
+var strong: bool = true #Number: Int and Float
 
 # The function "_ready():" is executed whe the object is ready 
 func _ready():
 	print("Hello, world!")
 	# Same mode of JavaScript to concatenate
-	print("I'm studying " + engine) 
 	print(engine.is_empty())
 	print(engine.length())
 	print(engine.to_upper()) 
-# This is a comment
+	# This is a comment
+	print(msg02 + "score")
 
---
+++ day-02/variables.gd
@@ -0,0 +1,12 @@
+extends Node
+
+var string: String = "String"
+var inter: int = 123
+var float_var: float = 1.23
+var boolean: bool = true
+
+func _ready():
+	print(string)
+	print(inter)
+	print(float_var)
+	print(boolean)
