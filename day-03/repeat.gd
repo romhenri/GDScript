871279a4bdf3b284dea009e4b1047285726f9df1
Rômulo Henri <123867521+romhenri@users.noreply.github.com>
2023-06-20 08:35:52 -0300

    study: day-04

--- day-03/repeat.gd
+++ day-03/repeat.gd
@@ -15,3 +15,16 @@ func _ready():
 	
 	if lista.size() == 3:
 		print("Lista tem 3 elementos.")
+	
+	for i in range(1, 4): # 1, 2, 3
+		print(i)
+	
+	print("De 10 em 10")
+	
+	for i in range(0, 101, 10): # 10, 20, 30, 40, 50, 60, 70, 80, 90, 100...
+		print(i)
+	
+	print("Agora decrementando...")
+	
+	for i in range(50, 0, -10): # 50, 40, 30, 20, 10...
+		print(i)
