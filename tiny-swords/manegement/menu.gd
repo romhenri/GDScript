531798f3f78e35fa0cd5399e5ac5fb430934616a
Rômulo Henri <123867521+romhenri@users.noreply.github.com>
2023-07-01 21:56:16 -0300

    study: day-14

--- tiny-swords/manegement/menu.gd
+++ tiny-swords/manegement/menu.gd
@@ -1,18 +1,18 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	for button in get_tree().get_nodes_in_group("btn"):
 		button.pressed.connect(on_button_pressed.bind(button.name))
 
 func on_button_pressed(button_name: String) -> void:
-	print(button_name)
+	# print(button_name)
 	
 	match button_name:
-		
 		"Start":
-			get_tree().change_scene_to_file("res://tiny-swords/manegement/level.tscn")
+			print("Start")
+			transition_screen.scene_path = "res://tiny-swords/manegement/level.tscn"
+			transition_screen.fade_in()
 			
 		"Quit":
 			get_tree().quit()
